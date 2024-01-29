ef647e7181c2bb3acdbee503da66d5d11e48c07d
lucas <lucasluz1710@gmail.com>
2024-01-29 12:36:20 -0300

    creating the global and variable styles to the pages

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,8 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Home } from "./pages/home.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import "./styles/variables.module.css";
+import "./styles/global.module.css";
+
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-
+    <Home/>
   </React.StrictMode>,
-)
+);
